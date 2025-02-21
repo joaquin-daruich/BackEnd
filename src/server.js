7e660b156c32d9854a1e78cc7c22b84a289704b9
joaquin-daruich <joaquindaruichh@gmail.com>
2025-02-20 22:42:30 -0300

    adelanto5

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 const app = express();
 
+app.use(express.json({limit: '0.2mb'}))
+app.use(express.urlencoded({extended:true}))
 const PUERTO = 7000
 
 app.get('/' , (req , res) => {
@@ -11,10 +13,18 @@ app.get('/' , (req , res) => {
 })
 
 
-app.use(cors()) 
-app.post('/prueba' , (req , res ) => {
-    console.log('funciona dicen')
-})
+const corsOptions = {
+    origin: 'https://trabajo-wp.vercel.app', 
+    methods: ['GET', 'POST'], 
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+  };
+  
+  app.use(cors(corsOptions));  
+  
+  app.post('/prueba', (req, res) => {
+    console.log('funciona dicen');
+    res.send('Solicitud recibida');
+  });
 
 app.use('/api/status' , statusRouter)
 app.listen( PUERTO, () => {
