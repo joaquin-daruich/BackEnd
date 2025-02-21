c34e81cfca6e5d7d49ecc9a9575f9ade80c5567e
joaquin-daruich <joaquindaruichh@gmail.com>
2025-02-21 17:20:17 -0300

    Rei II

--- src/server.js
+++ src/server.js
@@ -20,9 +20,12 @@ conexionDB();
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-    origin: 'https://trabajo-wp.vercel.app', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: 'http://localhost:5173',  // Cambia al puerto correcto de tu frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
+  credentials: true,
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
 };
 
 app.use(cors(corsOptions));
