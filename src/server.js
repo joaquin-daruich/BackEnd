fede443cca7f6af4409bdf5dbe7779b3b6d26602
joaquin-daruich <joaquindaruichh@gmail.com>
2025-02-20 21:28:24 -0300

    adelanto2

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
 import express from 'express'
+import statusRouter from './router.js';
 const app = express();
 
 const PUERTO = 3000
 
+app.get('/' , (req , res) => {
+    res.render('holaaa')
+})
+
+app.use('/api/status' , statusRouter)
 app.listen( PUERTO, () => {
     console.log('holaaa')
 } )
