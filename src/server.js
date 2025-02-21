f0e9488572aa87a8fdaabdc70126eb34b9a01bc6
joaquin-daruich <joaquindaruichh@gmail.com>
2025-02-20 22:12:39 -0300

    adelanto4

--- src/server.js
+++ src/server.js
@@ -1,11 +1,16 @@
 import express from 'express'
 import statusRouter from './router.js';
+import mongoose from 'mongoose'
 const app = express();
 
 const PUERTO = 7000
 
 app.get('/' , (req , res) => {
     res.render('holaaa')
+})
+
+app.post('/prueba' , (req , res ) => {
+    console.log('funciona dicen')
 })
 
 app.use('/api/status' , statusRouter)
